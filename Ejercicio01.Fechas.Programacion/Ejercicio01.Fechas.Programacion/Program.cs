256e088d2fa775c65c5c5303619476e20d8c3247
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-01-29 13:45:55 +0100

    Add files via upload

---
+++ Ejercicio01.Fechas.Programacion/Ejercicio01.Fechas.Programacion/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace Ejercicio01.Fechas.Programacion
+{
+
+
+
+
+    class Program
+    {
+
+       public static void Main(string[] args)
+        {
+
+
+
+            // Fecha Del Sistema
+            DateTime fechaDelSistema = DateTime.Now;
+            Console.WriteLine(fechaDelSistema);
+
+
+            // Formateo de la fecha
+            DateTimeFormatInfo formatoEspañol = CultureInfo.GetCultureInfo("en-ES").DateTimeFormat;
+            Type tipoFormato = formatoEspañol.GetType();
+            PropertyInfo[] proiedadesFormatoEspanol = tipoFormato.GetProperties(); 
+        
+
+            foreach (var propiedadFecha in proiedadesFormatoEspanol)
+            {
+
+                if (propiedadFecha.Name.Contains("Pattern"))
+                {
+
+                    string formaFecha = propiedadFecha.GetValue(formatoEspañol, null).ToString();
+                    Console.WriteLine(propiedadFecha.Name + ": " + formaFecha + " " +
+                              fechaDelSistema.ToString(formaFecha));
+
+
+                }
+
+            }
+
+            //Formato Especifico
+            Console.WriteLine("Formato fecha-tiempo: " + fechaDelSistema);
+            
+
+            
+        }
+
+    }
+}
